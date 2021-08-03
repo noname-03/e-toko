8d5f9908465c8e06ccb794574d3057e93a8c2dba
noname-03 <fahrurrozi.arul3@gmail.com>
2021-08-03 22:37:22 +0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('product', ProductController::class);
+Route::resource('categoty', CategoryController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
